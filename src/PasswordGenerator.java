61e91c3acb8dd2ebbff50ffb8fb560a415d4f9c9
fabrizioruiz96 <fabrizioruizzo@gmail.com>
2025-01-22 12:38:18 +0100

    feat: submit assignment

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
         
+        System.out.println("Inserisci il tuo nome");
+        String inputName = in.next();
+        
+        System.out.println("Inserisci il tuo cognome");
+        String inputSurname = in.next();
+        
+        System.out.println("Qual'è il tuo colore preferito?");
+        String inputColor = in.next();
+        
+        System.out.println("Inserisci la tua data di nascita");
+        System.out.println("Giorno: ");
+        int inputDay = in.nextInt();
+        System.out.println("Mese:(numero)");
+        int inputMonth = in.nextInt();
+        System.out.println("Anno:");
+        int inputYear = in.nextInt();
+
+        System.out.println("La tua password sarà: " + inputName + "-" + inputSurname + "-" + inputColor + "-" + (inputDay + inputMonth + inputYear));
     }
 }
